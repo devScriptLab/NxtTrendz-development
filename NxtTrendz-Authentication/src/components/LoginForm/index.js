97e835cb32984234d5051c6a4d22984d5dc08bac
Vikas Kamer <vikaskamera@duck.com>
2026-06-25 11:31:41 +0000

    Initial commit

---
+++ NxtTrendz-Authentication/src/components/LoginForm/index.js
@@ -0,0 +1,116 @@
+import {Component} from 'react'
+
+import './index.css'
+
+class LoginForm extends Component {
+  state = {
+    username: '',
+    password: '',
+    showSubmitError: false,
+    errorMsg: '',
+  }
+
+  onChangeUserName = event => this.setState({username: event.target.value})
+
+  onChangePassword = event => this.setState({password: event.target.value})
+
+  onSubmitSuccess = () => {
+    const {history} = this.props
+
+    history.replace('/')
+  }
+
+  onSubmitFailure = errorMsg => this.setState({showSubmitError: true, errorMsg})
+
+  submitForm = async event => {
+    event.preventDefault()
+    const {username, password} = this.state
+    const userDetails = {username, password}
+    const url = 'https://apis.ccbp.in/login'
+    const options = {
+      method: 'POST',
+      body: JSON.stringify(userDetails),
+    }
+
+    const response = await fetch(url, options)
+    const data = await response.json()
+    console.log(options)
+
+    if (response.ok === true) {
+      this.onSubmitSuccess()
+    } else {
+      this.onSubmitFailure(data.error_msg)
+    }
+  }
+
+  renderUserNameField = () => {
+    const {username} = this.state
+    return (
+      <div className="input-container">
+        <label htmlFor="username" className="input-label">
+          USERNAME
+        </label>
+        <input
+          id="username"
+          type="text"
+          className="input"
+          placeholder="Username"
+          onChange={this.onChangeUserName}
+          value={username}
+        />
+      </div>
+    )
+  }
+
+  renderPasswordField = () => {
+    const {password} = this.state
+    return (
+      <div className="input-container">
+        <label htmlFor="password" className="input-label">
+          PASSWORD
+        </label>
+        <input
+          id="password"
+          type="password"
+          className="input"
+          placeholder="Password"
+          onChange={this.onChangePassword}
+          value={password}
+        />
+      </div>
+    )
+  }
+
+  render() {
+    const {showSubmitError, errorMsg} = this.state
+    return (
+      <div className="login-form-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          alt="website logo"
+          className="login-website-logo-mobile-img"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-login-img.png"
+          alt="website login"
+          className="login-img"
+        />
+        <form className="form-container" onSubmit={this.submitForm}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+            alt="website logo"
+            className="login-website-logo-desktop-img"
+          />
+          {this.renderUserNameField()}
+          {this.renderPasswordField()}
+          <button type="submit" className="login-button">
+            Login
+          </button>
+          {showSubmitError && <p className="error-message">*{errorMsg}</p>}
+        </form>
+      </div>
+    )
+  }
+}
+
+export default LoginForm
